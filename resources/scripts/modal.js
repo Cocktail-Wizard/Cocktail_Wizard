2b432a9d8d99c18019b56175f88c8010a4e2253e
Vianpyro <vianney@veremme.org>
2024-03-01 07:42:17 -0500

    Utilisation d'un modèle à part pour la boite modale

--- resources/scripts/modal.js
+++ resources/scripts/modal.js
@@ -1,28 +1,45 @@
-// Get the modal
-var modal = document.getElementById("modal-wrapper");
+const MODAL_TEMPLATE = "resources/templates/cocktail_modal.html";
 
-// Get the <span> element that closes the modal
-var span = document.getElementById("close");
+// Récuperation de la section de la boite modale dans le DOM
+let sectionModale = document.getElementById("modal-wrapper");
 
 document.addEventListener("DOMContentLoaded", () => {
-    var gallery = document.getElementsByClassName('cocktail');
+    // Récuperation et utilisation du modèle de boite modale
+    fetch(MODAL_TEMPLATE)
+        .then(reponse => {
+            if (!reponse.ok) {
+                throw new Error("Impossible de récuperer le modèle.");
+            }
+            return reponse.text();
+        })
+        .then(data => {
+            sectionModale.innerHTML = data;
+        })
+        .then(() => {
+            // Récupération des éléments du template après qu'ils aient été ajoutés au DOM
+            let span = document.getElementById("close");
 
-    // When the user clicks on a cocktail, open the modal
+            span.onclick = () => {
+                sectionModale.style.display = "none";
+            }
+        })
+        .catch(error => {
+            console.error("Il y'a eu une erreur lors de la récuperation :", error);
+        });
+
+    let gallery = document.getElementsByClassName('cocktail');
+
+    // Quand un utilisateur clique sur un cocktail, ouvrir la boite modale
     Array.from(gallery).forEach((cocktail) => {
         cocktail.addEventListener('click', () => {
-            modal.style.display = "block";
+            sectionModale.style.display = "block";
         })
     })
-});
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = () => {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = (event) => {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    // Fermer la boite modale quant un utilisateur clique en dehors
+    window.onclick = (event) => {
+        if (event.target == sectionModale) {
+            sectionModale.style.display = "none";
+        }
     }
-}
+});
