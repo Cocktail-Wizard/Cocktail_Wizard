6f81044e8db91bda9d8baffcea65b4c8464f2417
Pablo <147773382+PabloCoroa@users.noreply.github.com>
2024-03-06 12:27:36 -0500

    Ajustement du lien CSS pour match le renommage du fichier CSS

--- Pages/pageConnexion.php
+++ Pages/pageConnexion.php
@@ -54,7 +54,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
-    <link rel="stylesheet" href="../ressources/styles/pageConnexion.css">
+    <link rel="stylesheet" href="../ressources/styles/connexion.css">
 </head>
 <body>
     <div id="connexion">
