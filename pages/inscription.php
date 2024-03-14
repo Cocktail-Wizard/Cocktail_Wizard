353c82ec78fb96c4644d9934bcf2c307a27f7580
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2024-03-14 15:45:34 -0400

    Initial commit

--- pages/inscription.php
+++ pages/inscription.php
@@ -1,123 +1,121 @@
 <?php
-    require("../ressources/api/connexion.php");
-    // Accumulateur d'erreurs
-    $erreurs = array();
+require("../ressources/api/connexion.php");
+// Accumulateur d'erreurs
+$erreurs = array();
 
-    if($_SERVER["REQUEST_METHOD"]=="POST")
-    {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        // Valider le nom d'utilisateur
-        if(empty($_POST['nom'])) {
-            $erreurs[] = "Le nom d'utilisateur est invalide!<br>";
+    // Valider le nom d'utilisateur
+    if (empty($_POST['nom'])) {
+        $erreurs[] = "Le nom d'utilisateur est invalide!<br>";
+    }
+
+    // Valider le courriel
+    if (empty($_POST['courriel']) || !str_contains($_POST["courriel"], "@") || !str_contains($_POST["courriel"], ".")) {
+        $erreurs[] = "Le courriel est invalide!<br>";
+    }
+
+    // Verifier si le courriel est deja utilise
+    $conn = connexion("cocktailwizbd.mysql.database.azure.com", "cocktail", "Cw-yplmv");
+    $courriel = mysqli_real_escape_string($conn, trim($_POST['courriel']));
+    if ($conn == null) {
+        die("Erreur");
+    }
+    $requete_preparee = $conn->prepare("SELECT * FROM utilisateur WHERE courriel = ?");
+    $requete_preparee->bind_param("s", $courriel);
+    $requete_preparee->execute();
+    $resultat = $requete_preparee->get_result();
+    $requete_preparee->close();
+    if ($resultat->num_rows > 0) {
+        $erreurs[] = "Le courriel est déjà utilisé!";
+        $conn->close();
+    }
+
+    // Valider le mot de passe
+    if (empty($_POST['mdp'])) {
+        $erreurs[] = "Le mot de passe est invalide!<br>";
+    } else {
+        $mdp = $_POST['mdp'];
+        // Valider la date de naissance
+        if (empty($_POST['naissance'])) {
+            $erreurs[] = "La date de naissance est invalide!<br>";
         }
-
-        // Valider le courriel
-        if(empty($_POST['courriel']) || !str_contains($_POST["courriel"],"@") || !str_contains($_POST["courriel"],".")) {
-            $erreurs[] = "Le courriel est invalide!<br>";
+        // regex nombre charactere
+        if (strlen($mdp) < 8) {
+            $erreurs[] = "Le mot de passe doit contenir au moins 8 caractères!<br>";
         }
+        // regex pour lettre min/max
+        if (!preg_match("/[a-z]/i", $mdp)) {
+            $erreurs[] = "Le mot de passe doit contenir au moins une minuscule!<br>";
+        }
+        // regex pour num
+        if (!preg_match("/[0-9]/", $mdp)) {
+            $erreurs[] = "Le mot de passe doit contenir au moins un chiffre!<br>";
+        }
+        // \W regex pour les caracteres speciaux
+        if (!preg_match("/\W/", $mdp)) {
+            $erreurs[] = "Le mot de passe doit contenir au moins un symbole!<br>";
+        }
+    }
 
-        // Verifier si le courriel est deja utilise
-        $conn = connexion("cocktailwizbd.mysql.database.azure.com","cocktail","Cw-yplmv");
+    //Verifier si le mot de passe est egale a la validation du mot de passe
+    if ($_POST['mdp'] != $_POST['confmdp']) {
+        $erreurs[] = "Les mots de passe ne sont pas identiques!<br>";
+    }
+
+    // Valider la date de naissance
+    if (empty($_POST['naissance'])) {
+        $erreurs[] = "La date de naissance est invalide!<br>";
+    }
+
+    // Afficher le message si le formulaire est valide
+    if (count($erreurs) == 0) {
+        $conn = connexion("cocktailwizbd.mysql.database.azure.com", "cocktail", "Cw-yplmv");
+        $nom = mysqli_real_escape_string($conn, trim($_POST['nom']));
         $courriel = mysqli_real_escape_string($conn, trim($_POST['courriel']));
-        if($conn == null)
-        {
+        $mdp = mysqli_real_escape_string($conn, trim($_POST['mdp']));
+        $mdp_encrypter = password_hash($mdp, PASSWORD_DEFAULT);
+        $date_nais = date('Y-m-d', strtotime(trim($_POST['naissance'])));
+        if ($conn == null) {
             die("Erreur");
         }
-        $requete_preparee = $conn->prepare("SELECT * FROM utilisateur WHERE courriel = ?");
-        $requete_preparee->bind_param("s", $courriel);
+        $requete_preparee = $conn->prepare("SELECT * FROM utilisateur WHERE nom = ?");
+        $requete_preparee->bind_param("s", $nom);
         $requete_preparee->execute();
         $resultat = $requete_preparee->get_result();
         $requete_preparee->close();
         if ($resultat->num_rows > 0) {
-            $erreurs[] = "Le courriel est déjà utilisé!";
+            $erreurs[] = "Le nom d'utilisateur est déjà utilisé!";
             $conn->close();
         }
 
-        // Valider le mot de passe
-        if(empty($_POST['mdp'])) {
-            $erreurs[] = "Le mot de passe est invalide!<br>";
-        }
-        else{
-            $mdp = $_POST['mdp'];
-            // Valider la date de naissance
-            if(empty($_POST['naissance'])) {
-                $erreurs[] = "La date de naissance est invalide!<br>";
-            }
-            // regex nombre charactere
-            if (strlen($mdp) < 8) {
-                $erreurs[] = "Le mot de passe doit contenir au moins 8 caractères!<br>";
-            }
-            // regex pour lettre min/max
-            if(!preg_match("/[a-z]/i", $mdp)) {
-                $erreurs[] = "Le mot de passe doit contenir au moins une minuscule!<br>";
-            }
-            // regex pour num
-            if(!preg_match("/[0-9]/", $mdp)) {
-                $erreurs[] = "Le mot de passe doit contenir au moins un chiffre!<br>";
-            }
-            // \W regex pour les caracteres speciaux
-            if(!preg_match("/\W/", $mdp)) {
-                $erreurs[] = "Le mot de passe doit contenir au moins un symbole!<br>";
-            }
-        }
-
-        //Verifier si le mot de passe est egale a la validation du mot de passe
-        if($_POST['mdp']!=$_POST['confmdp']) {
-            $erreurs[] = "Les mots de passe ne sont pas identiques!<br>";
-        }
-
-        // Valider la date de naissance
-        if(empty($_POST['naissance'])) {
-            $erreurs[] = "La date de naissance est invalide!<br>";
-        }
-
-        // Afficher le message si le formulaire est valide
-        if (count($erreurs) == 0) {
-            $conn = connexion("cocktailwizbd.mysql.database.azure.com","cocktail","Cw-yplmv");
-            $nom = mysqli_real_escape_string($conn, trim($_POST['nom']));
-            $courriel = mysqli_real_escape_string($conn, trim($_POST['courriel']));
-            $mdp = mysqli_real_escape_string($conn, trim($_POST['mdp']));
-            $mdp_encrypter = password_hash($mdp, PASSWORD_DEFAULT);
-            $date_nais = date('Y-m-d', strtotime(trim($_POST['naissance'])));
-            if($conn == null)
-            {
-                die("Erreur");
-            }
-            $requete_preparee = $conn->prepare("SELECT * FROM utilisateur WHERE nom = ?");
-            $requete_preparee->bind_param("s", $nom);
-            $requete_preparee->execute();
-            $resultat = $requete_preparee->get_result();
-            $requete_preparee->close();
-            if ($resultat->num_rows > 0) {
-                $erreurs[] = "Le nom d'utilisateur est déjà utilisé!";
+        // Inserer les donnees dans la base de donnee
+        else {
+            $requete_preparee = $conn->prepare("INSERT INTO utilisateur (nom,courriel,mdp,date_nais) VALUES (?,?,?,?)");
+            $requete_preparee->bind_param("ssss", $nom, $courriel, $mdp_encrypter, $date_nais);
+            if ($requete_preparee->execute()) {
+                $requete_preparee->close();
                 $conn->close();
-            }
-
-            // Inserer les donnees dans la base de donnee
-            else{
-                $requete_preparee = $conn->prepare("INSERT INTO utilisateur (nom,courriel,mdp,date_nais) VALUES (?,?,?,?)");
-                $requete_preparee->bind_param("ssss",$nom,$courriel,$mdp_encrypter,$date_nais);
-                if($requete_preparee->execute())
-                {
-                    $requete_preparee->close();
-                    $conn->close();
-                    header("Location: pageConnexion.php");
-                    exit();
-                }
+                header("Location: pageConnexion.php");
+                exit();
             }
         }
     }
+}
 
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription</title>
+    <link rel="stylesheet" href="../ressources/styles/main.css">
     <link rel="stylesheet" href="../ressources/styles/inscription.css">
 </head>
+
 <body id="conteneur-inscription">
     <div id="inscription">
         <h1>DEVENIR MEMBRE</h1>
@@ -138,7 +136,7 @@
             <img src="../ressources/images/LogoCW.png" id="LogoCW-inscription" alt="Logo Cocktail Wizard">
         </div>
         <div id="messErreur">
-            <?php if(count($erreurs)>0) { ?>
+            <?php if (count($erreurs) > 0) { ?>
                 <?php foreach ($erreurs as $erreur) { ?>
                     <p class="erreur"><?php echo $erreur; ?></p><br>
                 <?php } ?>
@@ -146,4 +144,5 @@
         </div>
     </div>
 </body>
+
 </html>
--- pages/pageConnexion.php
+++ pages/pageConnexion.php
@@ -1,64 +1,66 @@
 <?php
-    require("../ressources/api/connexion.php");
-    session_start();
-    // Accumulateur d'erreurs
-    $erreurs = array();
+require("../ressources/api/connexion.php");
+session_start();
+// Accumulateur d'erreurs
+$erreurs = array();
 
-    if($_SERVER["REQUEST_METHOD"]=="POST")
-    {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        // Valider le nom d'utilisateur
-        if(empty($_POST['nom'])) {
-            $erreurs[] = "Le nom d'utilisateur entré est invalide!<br>";
-        }
+    // Valider le nom d'utilisateur
+    if (empty($_POST['nom'])) {
+        $erreurs[] = "Le nom d'utilisateur entré est invalide!<br>";
+    }
 
-        // Valider le mot de passe
-        if(empty($_POST['mdp'])) {
-            $erreurs[] = "Le mot de passe entré est invalide!<br>";
-        }
+    // Valider le mot de passe
+    if (empty($_POST['mdp'])) {
+        $erreurs[] = "Le mot de passe entré est invalide!<br>";
+    }
 
-        // Si aucune erreur, etablir la connexion
-        if (count($erreurs) == 0) {
-            //  Etablir la connexion avec la base de donnée
-            $conn = connexion("cocktailwizbd.mysql.database.azure.com","cocktail","Cw-yplmv");
-            $nom = mysqli_real_escape_string($conn, trim($_POST['nom']));
-            $mdp = mysqli_real_escape_string($conn, trim($_POST['mdp']));
-            if($conn == null)
+    // Si aucune erreur, etablir la connexion
+    if (count($erreurs) == 0) {
+        //  Etablir la connexion avec la base de donnée
+        $conn = connexion("cocktailwizbd.mysql.database.azure.com", "cocktail", "Cw-yplmv");
+        $nom = mysqli_real_escape_string($conn, trim($_POST['nom']));
+        $mdp = mysqli_real_escape_string($conn, trim($_POST['mdp']));
+        if ($conn == null)
             die("Erreur");
 
-            //  Rechercher le mot de passe dans la base de donnée
-            $requete_preparee = $conn->prepare("SELECT mdp FROM utilisateur WHERE nom = ?");
-            //  Lié le mot de passe (String) à l'identifiant
-            $requete_preparee->bind_param("s", $nom);
-            $requete_preparee->execute();
-            $resultat = $requete_preparee->get_result();
-            $util = $resultat->fetch_assoc();
-            if($resultat->num_rows > 0 ){
+        //  Rechercher le mot de passe dans la base de donnée
+        $requete_preparee = $conn->prepare("SELECT mdp FROM utilisateur WHERE nom = ?");
+        //  Lié le mot de passe (String) à l'identifiant
+        $requete_preparee->bind_param("s", $nom);
+        $requete_preparee->execute();
+        $resultat = $requete_preparee->get_result();
+        $util = $resultat->fetch_assoc();
+        if ($resultat->num_rows > 0) {
             $mdp_encrypter = $util['mdp'];
-            }
-            $requete_preparee->close();
+        }
+        $requete_preparee->close();
 
-            if ($resultat->num_rows > 0 && password_verify($mdp,$mdp_encrypter)) {
-                $_SESSION['nom'] = $nom;
-                //  Rediriger l'utilisateur vers la page de galerie
-                header("Location: galerie.php");
-                exit();
-            }
+        if ($resultat->num_rows > 0 && password_verify($mdp, $mdp_encrypter)) {
+            $_SESSION['nom'] = $nom;
+            //  Rediriger l'utilisateur vers la page de galerie
+            header("Location: galerie.php");
+            exit();
         }
     }
+}
 
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
+    <link rel="stylesheet" href="../ressources/styles/main.css">
     <link rel="stylesheet" href="../ressources/styles/connexion.css">
 </head>
+
 <body>
     <div id="connexion">
-    <!-- Mettre le chemin appropriee une fois que l'organisation des fichiers soient etabli -->
+        <!-- Mettre le chemin appropriee une fois que l'organisation des fichiers soient etabli -->
         <img src="../ressources/images/LogoCW.png" id="logoCW" alt="Logo Cocktail Wizard">
         <form id="form-connexion" method="post">
             <h1>COCKTAIL WIZARD</h1>
@@ -69,7 +71,7 @@
             <button type="submit">Connexion</button>
             <p>Vous n'êtes pas encore membre?</p>
             <a href="./inscription.php">Créer un compte</a>
-            <?php if(count($erreurs) > 0){ ?>
+            <?php if (count($erreurs) > 0) { ?>
                 <?php foreach ($erreurs as $erreur) { ?>
                     <p class="erreur"><?php echo $erreur; ?></p><br>
                 <?php } ?>
@@ -79,4 +81,5 @@
         </form>
     </div>
 </body>
+
 </html>
