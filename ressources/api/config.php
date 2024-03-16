a738663e8ce6660ecdb292b8abf22dadf93a1f98
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2024-03-16 18:06:00 -0400

    Utilisation d'un fichier `.env` pour cacher les donnees sensibles

--- ressources/api/config.php
+++ ressources/api/config.php
@@ -1,9 +1,12 @@
 <?php
+// Charger les variables d'environnement depuis le fichier .env
+$env = parse_ini_file('../../.env');
+
 // Définir les constantes pour les informations sensibles de la base de données
-define('DB_HOST', 'cocktailwizbd.mysql.database.azure.com');
-define('DB_USER', 'cocktail');
-define('DB_PASSWORD', 'Cw-yplmv');
-define('DB_NAME', 'nom_de_votre_base_de_donnees');
+define('DB_HOST', $env['DB_HOST']);
+define('DB_USER', $env['DB_USER']);
+define('DB_PASSWORD', $env['DB_PASSWORD']);
+define('DB_NAME', $env['DB_NAME']);
 
 // Créer la connexion à la base de données directement
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
