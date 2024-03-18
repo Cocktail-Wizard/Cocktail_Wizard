44eeff8b1df68e9f7feb3fb3b00ba28f9ff83f39
Amelo32 <147934073+Amelo32@users.noreply.github.com>
2024-03-18 14:38:37 -0400

    Ajout du fichier getCocktail.php
    Ce fichier permet d'accéder et de renvoyer les informations de la base de donnée nécessaires à l'affichage des cocktails dans monbar, la galerie et la section Mes cocktails du profil.

---
+++ ressources/api/getCocktail.php
@@ -0,0 +1,32 @@
+<?php
+    require_once __DIR__ . '/ressources/api/config.php';
+    require_once __DIR__ . '/ressources/classephp/Cocktail_Classe.php';
+    require_once __DIR__ . '/ressources/classephp/IngredientCocktail_Classe.php';
+    require_once __DIR__ . '/ressources/classephp/IngredientAlcool_Classe.php';
+    require_once __DIR__ . '/ressources/classephp/Commentaire_Classe.php';
+
+
+    function getCocktailGalerieNonfiltrer(){
+
+    }
+
+    function getCocktailGalerieFiltrer(){
+
+    }
+
+    function getCocktailClassiqueMonBar(){
+
+    }
+
+    function getCocktailFavorieMonBar(){
+
+    }
+
+    function getCocktailCommunataireMonBar(){
+
+    }
+
+    function getMesCocktailsProfil(){
+
+    }
+?>
