208d9dda9566ec0903b518ca511381b45c2c6a82
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2024-03-17 21:08:28 -0400

    Utilisation de la bdd

--- ressources/api/galerie.php
+++ ressources/api/galerie.php
@@ -6,16 +6,28 @@ $saveursUmami = ["Sucré", "Aigre", "Amer", "Épicé", "Salé"];
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     if (isset($_GET["nombre"])) {
-        for ($i = 0; $i < $_GET["nombre"]; $i++) {
-            $cocktails[] = array(
-                "id_cocktail" => $i,
-                "nom" => "Cocktail " . $i + 1,
-                "nb_likes" => rand(0, 1000),
-                "date_publication" => new DateTime(),
-                "umami" => $saveursUmami[$i % count($saveursUmami)]
-            );
+        $requete_preparee = $conn->prepare("SELECT id_cocktail, nom, nb_like, date_publication, profil_saveur FROM Cocktail LIMIT ?");
+        $requete_preparee->bind_param("i", $_GET["nombre"]); // Utiliser "i" pour un paramètre entier
+        $requete_preparee->execute();
+        $resultat = $requete_preparee->get_result();
+
+        $cocktails = array(); // Initialiser le tableau des cocktails
+
+        if ($resultat->num_rows > 0) {
+            // Parcourir les résultats de la requête et construire un tableau associatif pour chaque cocktail
+            while ($row = $resultat->fetch_assoc()) {
+                $profil_saveur = intval($row["profil_saveur"]);
+                $cocktail = array(
+                    "id_cocktail" => $row["id_cocktail"],
+                    "nom" => $row["nom"],
+                    "nb_likes" => $row["nb_like"],
+                    "date_publication" => $row["date_publication"],
+                    "umami" => $saveursUmami[$profil_saveur % count($saveursUmami)]
+                );
+                $cocktails[] = $cocktail; // Ajouter le cocktail au tableau des cocktails
+            }
         }
 
-        echo json_encode($cocktails);
+        echo json_encode($cocktails); // Encoder le tableau des cocktails en JSON et l'envoyer comme réponse
     }
 }
