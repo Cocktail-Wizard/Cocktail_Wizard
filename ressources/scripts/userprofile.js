f65ceb31b5f9547a2c9db8fd5d6542ad42667cab
AdelnorAcmorux <leonard.marcoux03@gmail.com>
2024-03-21 11:47:50 -0400

    la page modal multi page marche

--- ressources/scripts/userprofile.js
+++ ressources/scripts/userprofile.js
@@ -1,15 +1,44 @@
 // Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// Get the buttons that open the modal pages
+var infoBtn = document.getElementById("infoBtn");
+var cocktailBtn = document.getElementById("cocktailBtn");
+var supportBtn = document.getElementById("supportBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal
-btn.onclick = function () {
+// Get the content pages
+var infoPage = document.getElementById("infoPage");
+var cocktailPage = document.getElementById("cocktailPage");
+var supportPage = document.getElementById("supportPage");
+
+// Function to show modal and corresponding page
+function openModal(page) {
     modal.style.display = "block";
+    infoPage.style.display = "none";
+    cocktailPage.style.display = "none";
+    supportPage.style.display = "none";
+    page.style.display = "block";
+}
+
+// Open the modal with the Info personnelle page when the Profile User button is clicked
+document.getElementById("myBtn").onclick = function () {
+    openModal(infoPage);
+}
+
+// When the user clicks on the buttons to open corresponding pages
+infoBtn.onclick = function () {
+    openModal(infoPage);
+}
+
+cocktailBtn.onclick = function () {
+    openModal(cocktailPage);
+}
+
+supportBtn.onclick = function () {
+    openModal(supportPage);
 }
 
 // When the user clicks on <span> (x), close the modal
