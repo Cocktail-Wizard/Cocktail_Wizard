8387bbc539ef0d62321a0d450ffbbfc48f485715
Amelo32 <147934073+Amelo32@users.noreply.github.com>
2024-03-22 17:19:33 -0400

    Ajout du framework bootstrap
    w

--- pages/connexion.php
+++ pages/connexion.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion</title>
+    <title>Cocktail Wizard</title>
     <link rel="stylesheet" href="../ressources/styles/main.css">
     <link rel="stylesheet" href="../ressources/styles/connexion.css">
 </head>
--- pages/monbar.php
+++ pages/monbar.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mon bar</title>
+    <title>Cocktail Wizard</title>
     <link rel="stylesheet" href="../ressources/styles/main.css">
     <link rel="stylesheet" href="../ressources/styles/monbar.css">
     <link rel="stylesheet" href="../ressources/styles/carte_cocktail.css">
