ae6b4a0df6d602db235102e0b9eddd995c014fdf
Amelo32 <147934073+Amelo32@users.noreply.github.com>
2024-03-23 00:29:08 -0400

    Changement routage du fichier configDonne

--- ressources/api/config.php
+++ ressources/api/config.php
@@ -1,6 +1,6 @@
 <?php
 function connexionBD(){
-    require_once (__DIR__.'/configDonne.php');
+    require_once (__DIR__.'/../../configDonne.php');
 
     // Créer la connexion à la base de données directement
     $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
