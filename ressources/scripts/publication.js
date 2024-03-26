3808af6867f5e07fe23dd57fd6c53ea3b82de8ce
AdelnorAcmorux <leonard.marcoux03@gmail.com>
2024-03-26 09:36:46 -0400

    ajout/modification commentaire + traduction

--- ressources/scripts/publication.js
+++ ressources/scripts/publication.js
@@ -1,6 +1,5 @@
 const allIngredients = ["Vodka", "Rhum", "Gin", "Tequila", "Whiskey", "Triple sec", "Sirop simple", "Jus de citron", "Jus de lime", "Jus d'orange", "Jus de canneberge", "Grenadine", "Jus de pomme", "Jujutsu Kaisen"];
 
-
 let ingredientName = "";
 let ingredientAmount = "";
 let ingredientUnit = "";
@@ -12,75 +11,75 @@ function addIngredientToList(name, amount, unit) {
     listItem.textContent = `${amount} ${unit} ${name} `;
     ingredientList.appendChild(listItem);
 
-    // Clear the text areas
+    // Effacer les zones de texte SEULMENT quand un ingredient est rajouté
     document.getElementById("ingredient_name").value = "";
     document.getElementById("ingredient_amount").value = "";
 }
 
 
-// Get the modal
+// Récupérer le modal
 var modal = document.getElementById("myModal");
 
-// Get the <span> element that closes the modal
+// Récupérer l'élément qui ferme le modal
 var span = document.getElementsByClassName("close")[0];
 
-// Function to show modal and corresponding page
+// Fonction pour afficher le modal et la page de publication
 function openModal(page) {
     modal.style.display = "block";
     page.style.display = "block";
 
-    // Remplir les champs de saisie avec les valeurs précédemment saisies
+    // Remplir les champs de saisie avec les valeurs précédemment saisies (garder les ingredients si l'utilisatuer ferme la page modale)
     document.getElementById("ingredient_name").value = ingredientName;
     document.getElementById("ingredient_amount").value = ingredientAmount;
     document.getElementById("unit_select").value = ingredientUnit;
 
-    // Set "oz" as the default unit
+    // Définir "oz" comme unité par défaut
     document.getElementById("unit_select").value = "oz";
 }
 
-// Open the modal with the Info personnelle page when the Profile User button is clicked
+// Ouvrir le modal de publication
 document.getElementById("myBtn").onclick = function () {
     openModal(publicationPage);
 }
 
-// When the user clicks on <span> (x), close the modal
+// Lorsque l'utilisateur clique sur <span> (x), fermer le modal
 span.onclick = function () {
     modal.style.display = "none";
 }
 
-// Prevent modal from closing and form submission when clicking on the "+" button
+// Evenement dans le formulaire lors du clic sur le bouton "+"
 document.getElementById("add_ingredient").addEventListener("click", function (event) {
-    // Get the value of the ingredient name input field
+    // Obtenir la valeur du champ de saisie du nom de l'ingrédient
     const enteredIngredient = document.getElementById("ingredient_name").value.trim().toLowerCase();
     const enteredAmount = document.getElementById("ingredient_amount").value.trim();
     const enteredUnit = document.getElementById("unit_select").value.trim();
 
-    // Check if the entered ingredient is in the list of all ingredients (case-insensitive)
+    // Vérifier si l'ingrédient saisi figure dans la liste de tous les ingrédients (insensible au minuscule/majuscule)
     if (!allIngredients.some(ingredient => ingredient.toLowerCase() === enteredIngredient)) {
-        // Change the color of the input field to red and apply the shake animation
+        // Changer la couleur du champ de saisie en rouge et appliquer l'animation d"erreur
         const ingredientInput = document.getElementById("ingredient_name");
         ingredientInput.classList.add("invalid-input");
 
-        // Remove the invalid-input class after the animation completes
+        // Supprimer la classe invalid-input après la fin de l'animation (pour pas que la case reste rouge)
         ingredientInput.addEventListener("animationend", removeInvalidClass);
     }
 
-    // Check if the entered amount is numerical
+    // Vérifier si la quantité saisie est numérique dans "enteredAmount"
     if (isNaN(parseFloat(enteredAmount))) {
-        // Change the color of the input field to red and apply the shake animation
+        // Changer la couleur du champ de saisie en rouge et appliquer l'animation d'erreur
         const amountInput = document.getElementById("ingredient_amount");
         amountInput.classList.add("invalid-input");
 
-        // Remove the invalid-input class after the animation completes
+        // Supprimer la classe invalid-input après la fin de l'animation
         amountInput.addEventListener("animationend", removeInvalidClass);
     }
 
-    // If both conditions are met, add the ingredient to the list
+    // Si les deux conditions sont remplies, ajouter l'ingrédient à la liste
     if (allIngredients.some(ingredient => ingredient.toLowerCase() === enteredIngredient) && !isNaN(parseFloat(enteredAmount))) {
         addIngredientToList(enteredIngredient, enteredAmount, enteredUnit);
     }
 
-    // Prevent the default behavior of the button (form submission or link navigation)
+    // Empêcher le comportement par défaut du bouton (pour ne pas fermer la modale)
     event.preventDefault();
 
 });
@@ -90,7 +89,7 @@ function removeInvalidClass(event) {
     event.target.classList.remove("invalid-input");
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// Lorsque l'utilisateur clique n'importe où en dehors du modal, le fermer
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
