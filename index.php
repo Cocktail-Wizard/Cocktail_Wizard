3fbd67070b7008f8836575d511df01d55f7874f2
Amelo32 <147934073+Amelo32@users.noreply.github.com>
2024-04-01 20:06:21 -0400

    separation html et ajout cookie

--- index.php
+++ index.php
@@ -8,92 +8,10 @@ if (!isset($_SESSION['username'])) {
 
 function galerieNonConnecte()
 {
-    echo '<!DOCTYPE html>
-    <html lang="en">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Cocktail Wizard</title>
-        <link rel="stylesheet" href="ressources/styles/main.css">
-        <link rel="stylesheet" href="ressources/styles/index.css">
-        <link rel="stylesheet" href="ressources/styles/modale.css">
-        <link rel="stylesheet" href="ressources/styles/carte_cocktail.css">
-        <script src="ressources/scripts/outils.js"></script>
-    </head>
-
-    <body>
-        <noscript class="erreur">Vous devez activer JavaScript pour charger ce site web.</noscript>
-
-        <header>
-            <h1>Cocktail Wizard</h1>
-        </header>
-
-        <main>
-            <nav><input id="barre-recherche" type="text" placeholder="Recherchez un cocktail..." autocomplete="off" autofocus></nav>
-
-            <section id="galerie"></section>
-        </main>
-
-        <aside id="contenant-modale"></aside>
-
-        <aside id="contenant-boutons-fixes">
-            <a href="/connexion" title="Connexion">
-                <img src="ressources/images/icone-profile.svg" alt="Icone profile">
-            </a>
-        </aside>
-
-        <footer>Cocktail Wizard &copy - 2024</footer>
-
-        <script src="ressources/scripts/index.js"></script>
-        <script src="ressources/scripts/modale.js"></script>
-    </body>
-
-    </html>';
+    require_once "pages/galerie.php";
 }
 
 function galerieConnecte()
 {
-    echo '<!DOCTYPE html>
-    <html lang="en">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Cocktail Wizard</title>
-        <link rel="stylesheet" href="ressources/styles/main.css">
-        <link rel="stylesheet" href="ressources/styles/index.css">
-        <link rel="stylesheet" href="ressources/styles/modale.css">
-        <link rel="stylesheet" href="ressources/styles/carte_cocktail.css">
-        <script src="ressources/scripts/outils.js"></script>
-    </head>
-
-    <body>
-        <noscript class="erreur">Vous devez activer JavaScript pour charger ce site web.</noscript>
-
-        <header>
-            <h1>Cocktail Wizard</h1>
-        </header>
-
-        <main>
-            <nav><input id="barre-recherche" type="text" placeholder="Recherchez un cocktail..." autocomplete="off" autofocus></nav>
-
-            <section id="galerie"></section>
-        </main>
-
-        <aside id="contenant-modale"></aside>
-
-        <aside id="contenant-boutons-fixes">
-            <a href="/connexion" title="Connexion">
-                <img src="ressources/images/icone-profile.svg" alt="Icone profile">
-            </a>
-        </aside>
-
-        <footer>Cocktail Wizard &copy - 2024</footer>
-
-        <script src="ressources/scripts/index.js"></script>
-        <script src="ressources/scripts/modale.js"></script>
-    </body>
-
-    </html>';
+    require_once "pages/galerieConn.php";
 }
