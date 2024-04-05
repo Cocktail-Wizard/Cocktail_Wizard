4d2989eb6a2acaee6cd5e4e3f41182d127fed08e
AdelnorAcmorux <leonard.marcoux03@gmail.com>
2024-04-05 12:19:49 -0400

    on met ca au milieu

--- pages/monbar.php
+++ pages/monbar.php
@@ -78,57 +78,58 @@
     <!-- la page modale -->
     <div id="my-modal" class="modal">
         <div class="modal-dialog modal-xl">
-        <!-- Contenue de la boite modale -->
-        <div id="userprofile-content" class="modal-content">
-            <div id="userprofile-header" class="modal-header">
-
-                <button id="info-btn">Info personnelle</button>
-                <button id="cocktail-btn">Mes cocktails</button>
-                <button id="support-btn">Support</button>
-                <button type="button" class="btn-close" data-bs-dismiss="modal">&times;</button>
+            <!-- Contenue de la boite modale -->
+            <div id="userprofile-content" class="modal-content">
+                <div class="modal-header">
+                    <div id="userprofile-header">
+                        <button id="info-btn">Info personnelle</button>
+                        <button id="cocktail-btn">Mes cocktails</button>
+                        <button id="support-btn">Support</button>
+                    </div>
+                    <button type="button" id="fuck-bootstrap" class="btn-close" data-bs-dismiss="modal"></button>
                     <!-- symbole x pour fermer -->
-            </div>
+                </div>
 
-            <div class="modal-body">
-            <div class="userinfo-body" id="infoPage">
-                <img src="../ressources/images/lionWizard.jpg" alt="Photo de profil de l'utilisateur">
-                <h3>Nom de l'utilisateur: Lionel Wizard</h3>
-                <p>Nombre de likes : <span id="likeCount">0</span></p>
-                <p>Nombre de cocktails : <span id="cocktailCount">0</span></p>
-                <p>Nombre de commentaires : <span id="commentCount">0</span></p>
-            </div>
+                <div class="modal-body">
+                    <div class="userinfo-body" id="infoPage">
+                        <img src="../ressources/images/lionWizard.jpg" alt="Photo de profil de l'utilisateur">
+                        <h3>Nom de l'utilisateur: Lionel Wizard</h3>
+                        <p>Nombre de likes : <span id="likeCount">0</span></p>
+                        <p>Nombre de cocktails : <span id="cocktailCount">0</span></p>
+                        <p>Nombre de commentaires : <span id="commentCount">0</span></p>
+                    </div>
 
-            <div class="mycocktail-body" id="mycocktailPage">
-                <div class="cocktail-container">
-                    <!-- Les cartes de cocktail seront ajoutées ici -->
+                    <div class="mycocktail-body" id="mycocktailPage">
+                        <div class="cocktail-container">
+                            <!-- Les cartes de cocktail seront ajoutées ici -->
+                        </div>
+                    </div>
+
+                    <div class="support-body" id="supportPage">
+
+                        <h2 style="color: #7f5af0;">Contactez-nous</h2>
+
+                        <form action="../ressources/email.php" method="post">
+                            <label for="message">Message:</label><br>
+                            <textarea id="message" name="message" rows="4" cols="50"></textarea><br>
+                            <input id="submit-btn" type="submit" value="Envoyer">
+                        </form>
+
+                    </div>
+                </div>
+                <div class="userprofile-footer">
+                    <h3>Cocktail Wizard &copy - 2024</h3>
                 </div>
             </div>
-
-            <div class="support-body" id="supportPage">
-
-                <h2 style="color: #7f5af0;">Contactez-nous</h2>
-
-                <form action="../ressources/email.php" method="post">
-                    <label for="message">Message:</label><br>
-                    <textarea id="message" name="message" rows="4" cols="50"></textarea><br>
-                    <input id="submit-btn" type="submit" value="Envoyer">
-                </form>
-
-            </div>
-            </div>
-            <div class="userprofile-footer">
-                <h3>Cocktail Wizard &copy - 2024</h3>
-            </div>
-        </div>
         </div>
     </div>
 
     <aside id="deux-boutons">
         <button>
-            <img id="btnAbout" src="../ressources/images/info.svg" alt="Question Mark" width="24" height="24">
+            <img id="btnAbout" src="../ressources/images/info.svg" alt="Info de la page">
         </button>
         <button>
-            <img id="btnPublish" src="../ressources/images/feather.svg" alt="Feather" width="24" height="24">
+            <img id="btnPublish" src="../ressources/images/feather.svg" alt="Publication">
         </button>
     </aside>
 
