2c224bb04056200b25a03a9369948dd056b0445d
Vianpyro <vianney@veremme.org>
2024-04-05 15:47:44 -0400

    test cookies

--- ressources/scripts/connexionWeb.php
+++ ressources/scripts/connexionWeb.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 require_once __DIR__ . "/../api/connexion.php";
 
@@ -7,7 +8,8 @@ $cookie_secure = true; // Only transmit cookie over HTTPS
 $cookie_http_only = false; // Prevent cookie from being accessed through JavaScript
 
 if ($success) {
-    session_start();
     $_SESSION['username'] = $nom;
     setcookie('username', $nom, 0, '/', $cookie_domain, $cookie_secure, $cookie_http_only);
 }
+
+echo json_encode($response);
