0f9c05479993ae4ba2293d56ff4534b868a6bc0b
Vianpyro <vianney@veremme.org>
2024-04-05 15:17:59 -0400

    Hotfix?

--- ressources/scripts/connexionWeb.php
+++ ressources/scripts/connexionWeb.php
@@ -2,8 +2,12 @@
 
 require_once __DIR__ . "/../api/connexion.php";
 
+$cookie_domain = ""; // Set to your domain if needed
+$cookie_secure = true; // Only transmit cookie over HTTPS
+$cookie_http_only = true; // Prevent cookie from being accessed through JavaScript
+
 if ($success) {
     session_start();
     $_SESSION['username'] = $nom;
-    setcookie('username', $nom, 0, '/', "", true, true);
+    setcookie('username', $nom, 0, '/', $cookie_domain, $cookie_secure, $cookie_http_only);
 }
