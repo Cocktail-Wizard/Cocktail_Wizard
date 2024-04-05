208011e1df684d8204da456795edf200f099eb25
Leonard Marcoux <140647944+AdelnorAcmorux@users.noreply.github.com>
2024-04-05 13:41:01 -0400

    Feature/publication cocktail (#94)
    * creation des fihier pour la publication des cocktails
    
    * voila, le scroolin gmarche mtn
    
    * push changes
    
    * rajouter des ingredient a la liste d,ingredients
    
    * le concept, marche, des petit truc a ajuster
    
    * les text area ne se refresh que lorsque 'un element est added
    
    * rajout section preparation
    
    * ptit fix
    
    * Utilisation des variables, reorganisation, commentaires et traductions
    
    * commentaires en francais
    
    * ajout/modification commentaire + traduction
    
    * ajout icon, btn enlever, function remove
    
    * ajout flavor profil, alcool principale, fix css
    
    * PUSH
    
    * gne
    
    * push
    
    * Fix
    
    * a
    
    * img
    
    * changes
    
    * images
    
    * image, again
    
    * iamge, my god, elle fait jsute pas apparaitre en fait
    
    * commentaires
    
    * ok mtn ca ferme... mais je peux pus recliquer sur PUBLISH
    
    * push enorme
    
    Co-authored-by: Amelo32 <Amelo32@users.noreply.github.com>
    Co-authored-by: Vianney Veremme <Vianpyro@users.noreply.github.com>
    
    * fix removeIngredient, optimisation
    
    Co-authored-by: Vianney Veremme <Vianpyro@users.noreply.github.com>
    
    * Utilisation de l'API pour obtenir la liste des ingredients
    
    * liste
    
    * tit fix css
    
    * test dropdown
    
    * omg ptn enfin, ca marche, jai fix la recherche dynamique
    
    * ptit fix de css
    
    * Btn enelver est fucked
    
    * ajout de la modal de publication et des bouton dans la gallerie connecter
    
    * final, cest fini, its done
    
    * Update pages/monbar.php
    
    * Revert "final, cest fini, its done"
    
    This reverts commit b55d6b9a79c832d063b88f5acd85deabb4eae3f0.
    
    * css
    
    * linter
    
    * linter v2
    
    * on vas dire que cest ca qui veux
    linter v3
    
    * voila voila
    
    * correction des alt
    
    ---------
    
    Co-authored-by: Amelo32 <147934073+Amelo32@users.noreply.github.com>
    Co-authored-by: Vianney Veremme <10519369+Vianpyro@users.noreply.github.com>
    Co-authored-by: Amelo32 <Amelo32@users.noreply.github.com>
    Co-authored-by: Vianney Veremme <Vianpyro@users.noreply.github.com>
    Co-authored-by: Vianpyro <vianney@veremme.org>

--- ressources/scripts/monbar.js
+++ ressources/scripts/monbar.js
@@ -12,44 +12,79 @@ const iconesUmami = {
     'default': 'point-interrogation'
 };
 
-const allIngredients = ['Vodka', 'Rhum', 'Gin', 'Tequila', 'Whiskey', 'Triple sec', 'Sirop simple', 'Jus de citron', 'Jus de lime', "Jus d'orange", 'Jus de canneberge', 'Grenadine', 'Jus de pomme', 'Jujutsu Kaisen'];
+// const allIngredients = await faireRequete('/api/ingredients');
+const allIngredients = ["Vodka", "Rhum", "Gin", "Tequila", "Whiskey", "Triple sec", "Sirop simple", "Jus de citron", "Jus de lime", "Jus d'orange", "Jus de canneberge", "Grenadine", "Jus de pomme", "Jujutsu Kaisen"];
 let selectedIngredients = [];
 
-//Liste des ingredients (vas eventuellement etre update avec la bd)
+/**
+ * Filtre et affiche les ingrédients en fonction de la valeur de recherche fournie.
+ * Met à jour la liste des ingrédients disponibles en fonction de la saisie dans la barre de recherche.
+ * Affiche les ingrédients filtrés dans une boîte déroulante et permet à l'utilisateur de sélectionner un ingrédient.
+ * Les ingrédients déjà sélectionnés ne sont pas inclus dans les résultats.
+ *
+ * @function filterIngredients
+ * @returns {void}
+ */
 function filterIngredients() {
-    const searchValue = document.getElementById('boite-recherche').value.toLowerCase();
+    const searchValue = document.getElementById('boite-recherche').value.toLowerCase().trim();
     const listeIngredients = document.getElementById('liste-ingredients');
-    listeIngredients.innerHTML = '';
 
-    //affiche la liste si la searchbar contient quelquechose
-    if (searchValue.trim() !== '') {
-        listeIngredients.style.display = 'block';
-    } else {
-        //cache le drop down si la barre de recherche est vide
-        listeIngredients.style.display = 'none';
-    }
+    // Affiche la liste uniquement si la recherche n'est pas vide
+    listeIngredients.style.display = searchValue !== '' ? 'block' : 'none';
 
-    // filtre les ingredient en fonctions de la rechercher ET des element deja selectionner
+    // filtre les ingrédients en fonction de la recherche et des ingrédients déjà sélectionnés
     const filteredIngredients = allIngredients.filter(ingredient => !selectedIngredients.includes(ingredient) && ingredient.toLowerCase().includes(searchValue));
 
-    //creation et ajout des ingredients selectionner a la boite d'ingredients
-    filteredIngredients.forEach(ingredient => {
-        const ingredientItem = document.createElement('div');
-        ingredientItem.textContent = ingredient;
-        ingredientItem.classList.add('ingredient-item');
-        ingredientItem.addEventListener('click', () => selectIngredient(ingredient));
-        listeIngredients.appendChild(ingredientItem);
-    });
+    // Efface le contenu précédent de la liste d'ingrédients
+    listeIngredients.innerHTML = '';
+
+    if (searchValue !== '') {
+        if (filteredIngredients.length === 0) {
+            // Affiche "Aucun résultat..." si aucun ingrédient n'est trouvé
+            const noResultItem = document.createElement('div');
+            noResultItem.textContent = 'Aucun résultat...';
+            listeIngredients.appendChild(noResultItem);
+        } else {
+            // Crée et ajoute les ingrédients filtrés à la liste d'ingrédients
+            filteredIngredients.forEach(ingredient => {
+                const ingredientItem = document.createElement('div');
+                ingredientItem.textContent = ingredient;
+                ingredientItem.classList.add('ingredient-item');
+                ingredientItem.addEventListener('click', () => selectIngredient(ingredient));
+                listeIngredients.appendChild(ingredientItem);
+            });
+        }
+    }
 }
 
-//fonction pour selctionner un ingredient
+
+// Ajoute un événement keyup à la barre de recherche pour filtrer les ingrédients à chaque frappe de touche
+document.getElementById('boite-recherche').addEventListener('keyup', filterIngredients);
+
+
+
+/**
+ * Sélectionne un ingrédient et le rajoute à la liste des ingrédients sélectionnés.
+ * Met à jour l'affichage des ingrédients sélectionnés et filtre les ingrédients disponibles pour éviter les doublons.
+ *
+ * @function selectIngredient
+ * @param {string} ingredient - L'ingrédient à sélectionner.
+ * @returns {void}
+ */
 function selectIngredient(ingredient) {
     selectedIngredients.push(ingredient);
     updateSelectedIngredients();
     filterIngredients(); //update le dropdown pour pas pouvoiur rajouter 100 fois la meme chose
 }
 
-//fonction pour deselectionenr un ingredient
+/**
+ * Désélectionne un ingrédient de la liste des ingrédients sélectionnés.
+ * Met à jour l'affichage des ingrédients sélectionnés et filtre les ingrédients disponibles.
+ *
+ * @function unselectIngredient
+ * @param {string} ingredient - L'ingrédient à désélectionner.
+ * @returns {void}
+ */
 function unselectIngredient(ingredient) {
     const index = selectedIngredients.indexOf(ingredient);
     if (index !== -1) {
@@ -59,22 +94,37 @@ function unselectIngredient(ingredient) {
     }
 }
 
-//fonction qui met a jour l'affichage des element selectionner
+/**
+ * Met à jour l'affichage des ingrédients sélectionnés dans la boîte dédiée.
+ * Affiche chaque ingrédient sélectionné avec la possibilité de le désélectionner.
+ * Assure que la boîte des ingrédients sélectionnés est visible même si elle est vide.
+ *
+ * @function updateSelectedIngredients
+ * @returns {void}
+ */
 function updateSelectedIngredients() {
-    const selectedIngredientsDiv = document.getElementById('selectedIngredients');
+    const selectedIngredientsDiv = document.getElementById('ingredients-selectionne');
     selectedIngredientsDiv.innerHTML = '';
 
     selectedIngredients.forEach(ingredient => {
         const ingredientBox = document.createElement('span');
         ingredientBox.textContent = ingredient;
-        ingredientBox.classList.add('selected-ingredient');
+        ingredientBox.classList.add('ingredients-selectionne');
         ingredientBox.addEventListener('click', () => unselectIngredient(ingredient));
         selectedIngredientsDiv.appendChild(ingredientBox);
     });
     selectedIngredientsDiv.style.display = 'flex'; // s'ssurer que la boîte des ingrédients sélectionnés est visible meme si vide
 }
 
-// Ferme la liste des ingrédients lorsque l'utilisateur clique à l'extérieur
+/**
+ * Écouteur d'événements qui se déclenche lorsqu'un clic est effectué n'importe où sur le document.
+ * Gère la fermeture de la liste des ingrédients déroulante si le clic est en dehors de la zone de recherche ou de la liste des ingrédients.
+ * Assure que la liste des ingrédients reste visible tant que l'utilisateur ne clique pas en dehors de celle-ci.
+ *
+ * @event click
+ * @param {Object} event - L'événement de clic déclenché.
+ * @returns {void}
+ */
 document.addEventListener('click', function (event) {
     const boiteRecherche = document.getElementById('boite-recherche');
     const listeIngredients = document.getElementById('liste-ingredients');
@@ -92,10 +142,30 @@ document.addEventListener('click', function (event) {
     }
 });
 
-// Initial call to display all ingredients
-filterIngredients();
-updateSelectedIngredients(); // Display initially selected ingredients
+/**
+ * Appel initial pour afficher tous les ingrédients disponibles et les ingrédients sélectionnés.
+ *
+ * @function initialSetup
+ * @returns {void}
+ */
+function initialSetup() {
+    filterIngredients(); // Appel initial pour afficher tous les ingrédients disponibles
+    updateSelectedIngredients(); // Affiche initialement les ingrédients sélectionnés
+}
 
+// Appels initiaux
+initialSetup();
+
+
+/**
+ * Écouteur d'événements qui se déclenche lorsque le DOM est entièrement chargé.
+ * Initialise les fonctionnalités de recherche d'ingrédients, charge le modèle HTML pour la carte de cocktail,
+ * récupère les données des cocktails depuis une API et affiche les cocktails sur la page.
+ *
+ * @event DOMContentLoaded
+ * @param {Object} event - L'événement DOMContentLoaded déclenché.
+ * @returns {void}
+ */
 document.addEventListener('DOMContentLoaded', async () => {
     inputRechercheIngredient = document.getElementById('boite-recherche');
     inputRechercheIngredient.addEventListener('input', filterIngredients);
@@ -116,6 +186,17 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
 });
 
+/**
+ * Affiche les cocktails sur la page en utilisant les données fournies et le modèle HTML spécifié.
+ * Chaque cocktail est affiché avec son nom, icône "j'aime", icône d'alcool principal, icône de profil gustatif,
+ * image, couleur de pastille alcool, nombre de mentions "j'aime", et la fonctionnalité pour ouvrir la boîte modale.
+ * Les cocktails sont ajoutés aux sections classiques, personnels et communautaires.
+ *
+ * @function afficherCocktails
+ * @param {Array} data - Les données des cocktails à afficher.
+ * @param {HTMLElement} modeleHTML - Le modèle HTML pour la carte de cocktail.
+ * @returns {void}
+ */
 function afficherCocktails(data, modeleHTML) {
     data.forEach((cocktail) => {
         const nouveauCocktail = document.createElement('article');
@@ -170,6 +251,13 @@ function afficherCocktails(data, modeleHTML) {
     });
 }
 
+/**
+ * Nettoie le nom du cocktail en supprimant tous les caractères spéciaux, sauf les lettres de l'alphabet (minuscules et majuscules) et les chiffres.
+ *
+ * @function nettoyerNomCocktail
+ * @param {string} nom - Le nom du cocktail à nettoyer.
+ * @returns {string} - Le nom du cocktail nettoyé.
+ */
 function nettoyerNomCocktail(nom) {
     return nom.replace(/[^a-zA-Z0-9]/g, '');
 }
---
+++ ressources/scripts/publication.js
@@ -0,0 +1,111 @@
+// const allIngredients = await faireRequete('/api/ingredients');
+const allIngredients = ["Vodka", "Rhum", "Gin", "Tequila", "Whiskey", "Triple sec", "Sirop simple", "Jus de citron", "Jus de lime", "Jus d'orange", "Jus de canneberge", "Grenadine", "Jus de pomme", "Jujutsu Kaisen"];
+const boutonAjouterIngredient = document.getElementById('ajouter-ingredient');
+const nouvelIngredientNom = document.getElementById("ingredient-nom");
+const nouvelIngredientQtt = document.getElementById("ingredient-quantite");
+const nouvelIngredientUnite = document.getElementById("ingredient-unit");
+
+const modal = document.getElementById('monModal');
+
+// Fonction pour ajouter un ingrédient à la liste d'ingrédients
+function addIngredientToList(name, amount, unit) {
+    const ingredientList = modal.querySelector("#liste-ingredients-publish");
+    const listItem = document.createElement("div");
+    const button = document.createElement('button');
+
+    button.classList.add('enlever_ingredient');
+    button.classList.add('button-publish');
+
+    const img = document.createElement('img');
+    img.classList.add('btn-icon');
+    img.src = '../ressources/images/minus.svg';
+    img.alt = '-';
+
+    // Ajouter l'image à l'intérieur du bouton
+    button.appendChild(img);
+
+    // Ajouter le bouton à l'élément listItem
+    listItem.appendChild(button);
+
+    // Ajouter l'élément listItem à la liste des ingrédients
+    ingredientList.appendChild(listItem);
+
+    button.addEventListener('click', function (event) {
+        removeIngredient(this, event);
+    });
+
+    listItem.appendChild(button);
+    listItem.appendChild(document.createTextNode(`${amount} ${unit} ${name}`));
+    ingredientList.appendChild(listItem);
+
+    // Effacer les zones de texte SEULMENT quand un ingredient est rajouté
+    nouvelIngredientNom.value = "";
+    nouvelIngredientQtt.value = "";
+    nouvelIngredientUnite.value = "";
+}
+
+// Evenement dans le formulaire lors du clic sur le bouton "+"
+boutonAjouterIngredient.addEventListener("click", function (event) {
+    // Obtenir la valeur du champ de saisie du nom de l'ingrédient
+    const enteredIngredient = nouvelIngredientNom.value.trim().toLowerCase();
+    const enteredAmount = nouvelIngredientQtt.value.trim();
+    const enteredUnit = nouvelIngredientUnite.value.trim();
+
+
+
+    // Vérifier si l'ingrédient saisi figure dans la liste de tous les ingrédients (insensible au minuscule/majuscule)
+    if (!allIngredients.some(ingredient => ingredient.toLowerCase() === enteredIngredient)) {
+        // Changer la couleur du champ de saisie en rouge et appliquer l'animation d"erreur
+        nouvelIngredientNom.classList.add("invalid-input");
+
+        // Supprimer la classe invalid-input après la fin de l'animation (pour pas que la case reste rouge)
+        nouvelIngredientNom.addEventListener("animationend", removeInvalidClass);
+    }
+
+    // Vérifier si la quantité saisie est numérique dans "enteredAmount"
+    if (isNaN(parseFloat(enteredAmount))) {
+        // Changer la couleur du champ de saisie en rouge et appliquer l'animation d'erreur
+        nouvelIngredientQtt.classList.add("invalid-input");
+
+        // Supprimer la classe invalid-input après la fin de l'animation
+        nouvelIngredientQtt.addEventListener("animationend", removeInvalidClass);
+    }
+
+    // Si les deux conditions sont remplies, ajouter l'ingrédient à la liste
+    if (allIngredients.some(ingredient => ingredient.toLowerCase() === enteredIngredient) && !isNaN(parseFloat(enteredAmount))) {
+        addIngredientToList(enteredIngredient, enteredAmount, enteredUnit);
+    }
+
+    // Empêcher le comportement par défaut du bouton (pour ne pas fermer la modale)
+    event.preventDefault();
+
+});
+
+
+function removeInvalidClass(event) {
+    event.target.classList.remove("invalid-input");
+}
+
+
+function previewImage(event) {
+    let input = event.target;
+    let reader = new FileReader();
+    reader.onload = function () {
+        let dataURL = reader.result;
+        let preview = document.getElementById('preview');
+        preview.src = dataURL;
+    };
+    reader.readAsDataURL(input.files[0]);
+}
+
+// function removeIngredient(button) {
+//     // Accéder à l'élément parent du bouton, qui est le conteneur de l'ingrédient
+//     console.log(button);
+//     const listItem = button.parentElement;
+//     // Supprimer cet élément de la liste d'ingrédients
+//     listItem.remove();
+// }
+function removeIngredient(element, event) {
+    event.preventDefault();
+    element.parentNode.remove();
+}
