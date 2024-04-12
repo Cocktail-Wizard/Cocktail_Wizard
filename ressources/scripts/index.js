8f95e8d42d97baca3afcd886be09357aa83edb1b
Vianpyro <vianney@veremme.org>
2024-04-12 14:18:26 -0400

    Correction formatage

--- ressources/scripts/index.js
+++ ressources/scripts/index.js
@@ -14,7 +14,6 @@ let modeleCarteCocktail;
 
 document.addEventListener('DOMContentLoaded', async () => {
 
-
     modeleCarteCocktail = await chargerModeleHTML('ressources/modeles/cocktail_carte.html');
 
     if (!modeleCarteCocktail) return;
@@ -113,7 +112,8 @@ function afficherCocktailsPerso(data, modeleHTML, divParent) {
         const imageCocktail = nouveauCocktail.querySelector('.illustration-cocktail');
         imageCocktail.src = 'https://equipe105.tch099.ovh/images?image=' + cocktail.img_cocktail;
         imageCocktail.loading = 'lazy';
-        if(cocktail.ingManquant !== null && cocktail.ingManquant > 0) {
+
+        if (cocktail.ingManquant !== null && cocktail.ingManquant > 0) {
             imageCocktail.style.filter = 'grayscale(100%)';
             const ingManquant = nouveauCocktail.querySelector('.ingredient-manquant');
             ingManquant.style.display = 'block';
@@ -253,7 +253,7 @@ async function chargerCommentairesModale(idCocktail) {
 
 async function chercherCocktail() {
     const recherche = barreRecherche.value.replace(/[^a-zA-Z0-9]/g, '_');
-    if(estSelect === false) {
+    if (estSelect === false) {
         const endpoint = recherche ? `/recherche/${recherche}` : '';
         const user = utilisateur ? '?user=' + utilisateur : '';
         const data = await faireRequete(`/api/cocktails/tri/${ordreCocktails}${endpoint}${user}`);
@@ -262,7 +262,7 @@ async function chercherCocktail() {
             afficherCocktails(data);
         }
     }
-    else if(estSelect === true && recherche) {
+    else if (estSelect === true && recherche) {
         const data = await faireRequete(`/api/users/${utilisateur}/cocktails/tri/${ordreCocktails}/recherche/${recherche}`);
         if (data) {
             galerie.innerHTML = '';
@@ -330,20 +330,20 @@ function chargerCommenter(id_cocktail) {
 
 document.querySelectorAll('input[type=radio]').forEach(radio => {
 
-        radio.addEventListener('mousedown', function(event) {
-            estSelect = this.checked;
-        });
+    radio.addEventListener('mousedown', function (event) {
+        estSelect = this.checked;
+    });
 
-        radio.addEventListener('click', function(event) {
-            if (estSelect) {
-                this.checked = false;
-                estSelect = false;
-                chercherCocktail();
-            }
-            else {
-                this.checked = true;
-                estSelect = true;
-                chercherCocktail();
-            }
-        });
+    radio.addEventListener('click', function (event) {
+        if (estSelect) {
+            this.checked = false;
+            estSelect = false;
+            chercherCocktail();
+        }
+        else {
+            this.checked = true;
+            estSelect = true;
+            chercherCocktail();
+        }
+    });
 });
