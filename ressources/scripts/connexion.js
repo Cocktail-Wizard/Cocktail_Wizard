02210dcb76dc5e79fd227259033e4f630bb618cd
Vianney Veremme <10519369+Vianpyro@users.noreply.github.com>
2024-04-14 20:01:48 -0400

    Optimization/optimisations diverses (#115)
    * Simplification du JS de la connexion
    
    * Meme optimisation pour l'inscription
    
    * Amelioration de l'efficacite de la reinitialisation de la modale et gestion des erreurs
    
    * Correction formatage

--- ressources/scripts/connexion.js
+++ ressources/scripts/connexion.js
@@ -1,5 +1,5 @@
 // JavaScript pour gérer la soumission du formulaire et afficher les erreurs
-document.getElementById('form-connexion').addEventListener('submit', (event) => {
+document.getElementById('form-connexion').addEventListener('submit', async (event) => {
     event.preventDefault();
 
     // Réinitialiser les erreurs
@@ -9,26 +9,25 @@ document.getElementById('form-connexion').addEventListener('submit', (event) => 
     // Envoyer les données du formulaire à l'API PHP
     let formData = new FormData(event.target);
 
-    fetch("/authentification", {
-        method: "POST",
-        body: formData
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                const username = data.username;
-                document.cookie = "username=" + username + "; path=/";
-
-                window.location.href = "/galerie";
-            } else {
-                data.errors.forEach(error => {
-                    let errorElement = document.createElement('p');
-                    errorElement.innerText = error;
-                    errorsContainer.appendChild(errorElement);
-                });
-            }
-        })
-        .catch(error => {
-            console.error('Erreur lors de la soumission du formulaire:', error);
+    try {
+        const response = await fetch("/authentification", {
+            method: "POST",
+            body: formData
         });
+        const data = await response.json();
+
+        if (data.success) {
+            const { username } = data;
+            document.cookie = `username=${username}; path=/`;
+            window.location.href = "/galerie";
+        } else {
+            data.errors.forEach(error => {
+                let errorElement = document.createElement('p');
+                errorElement.textContent = error;
+                errorsContainer.appendChild(errorElement);
+            });
+        }
+    } catch (error) {
+        console.error('Erreur lors de la soumission du formulaire:', error);
+    }
 });
